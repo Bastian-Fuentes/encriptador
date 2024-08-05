459811ac011ec68dcc765b2a2aa897f5bbab41bf
Bastian-Fuentes <bastian.fuentesrojas2001@gmail.com>
2024-08-04 22:26:26 -0400

    Primer push

---
+++ script.js
@@ -0,0 +1,49 @@
+function encriptar() {
+    const text = document.getElementById("inputText").value;
+
+    let encryptedText = text
+        .replace(/e/g, "enter")
+        .replace(/i/g, "imes")
+        .replace(/a/g, "ai")
+        .replace(/o/g, "ober")
+        .replace(/u/g, "ufat");
+    document.getElementById("outputText").value = encryptedText;
+
+    const redirectLink = checkRedirection(text);
+    if (redirectLink) {
+        window.location.href = redirectLink;
+        return;
+    }
+}
+
+function desencriptar() {
+    const text = document.getElementById("inputText").value;
+    let decryptedText = text
+        .replace(/enter/g, "e")
+        .replace(/imes/g, "i")
+        .replace(/ai/g, "a")
+        .replace(/ober/g, "o")
+        .replace(/ufat/g, "u");
+    document.getElementById("outputText").value = decryptedText;
+}
+
+function copyText() {
+    const outputText = document.getElementById("outputText");
+    outputText.select();
+    document.execCommand("copy");
+    alert("Texto copiado: " + outputText.value);
+}
+
+function checkRedirection(text) {
+    for (let i = 0; i < redirectionArray.length; i++) {
+        if (text === redirectionArray[i][0]) {
+            return redirectionArray[i][1];
+        }
+    }
+    return null;
+}
+
+const redirectionArray = [
+    ["rick", "https://www.youtube.com/watch?v=dQw4w9WgXcQ"],
+    ["nightcore", "https://www.youtube.com/watch?v=cvaIgq5j2Q8"]
+];
